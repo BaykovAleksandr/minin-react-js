75c485dd99d92088eef03deb51645f8144cb8c04
Baykov Aleksandr <baykov-1988@mail.ru>
2025-07-01 21:08:40 +0300

    add a few buttons

--- react-vite-mini-project/src/App.jsx
+++ react-vite-mini-project/src/App.jsx
@@ -2,12 +2,16 @@ import Header from "./components/Header";
 
 import TeachingSection from "./components/TeachingSection.jsx";
 import DifferenceSection from "./components/DifferenceSection.jsx";
+import IntroSection from './components/IntroSection.jsx';
+import TabsSection from './components/TabsSection.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
+        <IntroSection />
+        <TabsSection />
         <TeachingSection />
         <DifferenceSection />
       </main>
---
+++ react-vite-mini-project/src/components/IntroSection.jsx
@@ -0,0 +1,15 @@
+export default function IntroSection() {
+  return (
+    <section>
+      <h1 className="centered">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, debitis?
+      </h1>
+      <h3 className="centered" style = {{color: '#666'}}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, harum
+        necessitatibus? Aliquid dolor accusantium obcaecati magnam, dolore amet
+        dicta exercitationem, excepturi quam rem, soluta consectetur? Cupiditate
+        excepturi perspiciatis vero libero.
+      </h3>
+    </section>
+  );
+}
