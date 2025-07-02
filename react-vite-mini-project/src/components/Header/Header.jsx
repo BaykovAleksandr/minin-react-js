b625adbbfd95821250d477a179a5712db9f1bed5
Baykov Aleksandr <baykov-1988@mail.ru>
2025-07-02 23:49:12 +0300

    add styled-components

--- react-vite-mini-project/src/components/Header/Header.jsx
+++ react-vite-mini-project/src/components/Header/Header.jsx
@@ -1,6 +1,17 @@
 import { useState, useEffect } from "react";
 import logo from "/logo-name.svg";
-import "./Header.css"
+//import "./Header.css";
+import {styled} from "styled-components";
+
+const HeaderComponent = styled.header`
+  height: 50px;
+  display: flex;
+  padding: 0 2rem;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  background: #fafafa;
+`;
 
 export default function Header() {
   const [dateType, setDateType] = useState(new Date());
@@ -13,10 +24,10 @@ export default function Header() {
 
   return (
     <div>
-      <header>
+      <HeaderComponent>
         <img src={logo} alt={"result"} />
         <span>Здесь будет время: {dateType.toLocaleTimeString()}</span>
-      </header>
+      </HeaderComponent>
     </div>
   );
 }
